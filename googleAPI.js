8fe80b9cb166c124d5319d15e328df43678773b8
Tomislav Raguž <tomislav.raguz@outlook.com>
2017-05-10 11:25:38 +0200

    Local storage

--- googleAPI.js
+++ googleAPI.js
@@ -1,6 +1,9 @@
+var markers = JSON.parse(localStorage.getItem("markers"));
+
+
 function initMap() {
         var uluru = {lat: 45.3430556, lng: 14.4091667};
-        var map = new google.maps.Map(document.getElementById('map'), {
+         map = new google.maps.Map(document.getElementById('map'), {
           zoom: 8,
           center: uluru
         });
@@ -27,5 +30,24 @@ function placeMarker(latLng, map) {
     infowindow.setContent(this.content);
     infowindow.open(map, this);
   });
+    markers.push(latLng)
 }
 
+function ucitajPodatke(){
+    for(var i = 0; i < markers.length ; i++){
+    var marker = new google.maps.Marker({
+    position: markers[i],
+    map: map,
+    content: "ayyy"
+    });
+    marker.addListener("click", function(){
+    var infowindow = new google.maps.InfoWindow();
+    infowindow.setContent(this.content);
+    infowindow.open(map, this);
+    });
+    }
+}
+
+function spremiPodatke(){
+    localStorage.setItem("markers", JSON.stringify(markers));
+}
